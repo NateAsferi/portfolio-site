37ecf482078bee3bda1569c42060d1d9d5d688c7
Nathan Asferi <149350044+NateAsferi@users.noreply.github.com>
2025-10-30 14:25:17 -0400

    Remove all project files

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-const repoName = 'portfolio-site';
-
-export default defineConfig({
-  base: `/${repoName}/`,
-  plugins: [react()],
-});
